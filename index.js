8a069872b56ed2f84eab0838373a4de28fe2176f
tomi <tomiwest@hotmail.com>
2023-02-09 12:38:44 +0200

    servertest

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('hello')
+import express from 'express'
+
+import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+dotenv.config()
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.status(200).send('Hello World!')
+})
+
+app.listen(process.env.PORT, () => console.log(process.env.PORT))
+
+export default app
