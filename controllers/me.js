de8edda6b8ec726ede0e12d8dfe6acc58b784c2f
tomi <tomiwest@hotmail.com>
2023-03-11 11:41:19 +0200

    add update name

--- controllers/me.js
+++ controllers/me.js
@@ -6,4 +6,19 @@ router.get('/', async (req, res) => {
   res.status(200).json(user)
 })
 
+router.put('/', async (req, res) => {
+  let updateField = {}
+
+  if (req.body.name) {
+    updateField = {
+      name: req.body.name,
+    }
+  }
+  await User.update(updateField, {
+    where: {
+      id: req.user.id,
+    },
+  })
+  res.status(201).json('Profile Updated')
+})
 module.exports = router
